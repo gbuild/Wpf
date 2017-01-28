9e0243f4d1e9e1ab727e9029b600025dabff7d7b
gbuild <vesgostream@yandex.ru>
2017-01-28 23:49:01 +0500

    TicTacToe start

---
+++ TicTacToe/TicTacToe/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TicTacToe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Private Members
+        /// <summary>
+        /// Содержание текущего результата игры
+        /// </summary>
+        private MarkType[] mResults;
+        /// <summary>
+        /// Истина если первый игрок играет Х или второй за O
+        /// </summary>
+        private bool mPlayer1Turn;
+        /// <summary>
+        /// Истина если игра закончена
+        /// </summary>
+        private bool mGameEnded;
+        #endregion
+        #region Constructor
+
+        /// <summary>
+        ///  Default constructor
+        /// </summary>
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            NewGame();
+        }
+
+        #endregion
+
+        private void NewGame()
+        {
+            mResults = new MarkType[9];
+        }
+    }
+}
---
+++ TicTacToe/TicTacToe/MarkType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+/// <summary>
+/// Тип значения клетки в текущей игре
+/// </summary>
+    public enum MarkType
+    {
+        /// <summary>
+        /// Клетка еще не заполнена
+        /// </summary>
+        Free,
+        /// <summary>
+        /// Значение клетки O
+        /// </summary>
+        Nought,
+        /// <summary>
+        /// Значение клетки X
+        /// </summary>
+        Cross
+    }
+}
